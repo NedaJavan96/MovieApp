6a88f1d5508d560694b657ef31db9fc544d6daa9
NedaJavan <nedajavan747@gmail.com>
2021-06-03 20:01:22 +0430

    working on splash animation.

--- MovieApp/app/src/main/java/com/example/movieapp/splash/SplashActivity.kt
+++ MovieApp/app/src/main/java/com/example/movieapp/splash/SplashActivity.kt
@@ -1,12 +1,36 @@
 package com.example.movieapp.splash
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.constraintlayout.motion.widget.MotionLayout.TransitionListener
 import com.example.movieapp.R
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        var flag = false
+
+        motion_layout.setTransitionListener(object : TransitionListener {
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {}
+
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
+                if (p1 == R.id.end && p2 == R.id.end2) {
+                    flag=true
+                }
+            }
+
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {}
+
+            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
+                if (!flag) {
+                    motion_layout.setTransition(R.id.end, R.id.end2)
+                    motion_layout.transitionToEnd()
+                }
+            }
+        })
+        motion_layout.transitionToEnd()
     }
 }
